1865820c65d3d2c477df0a085e4244ae1cd73c8a
Adam <adamk@lavabs.com>
2019-04-04 18:20:21 +0200

    Porzadki w main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,21 +3,28 @@
 
 #define DHT11_PIN 4 //do zmiany
 
+float wilgotnosc;
+float temperatura;
+
 DHT dht;
 
 void setup() {
   // Tutaj umieszczamy wszystkie funkcje, ktore
   // Arduino ma wykonac zaraz po starcie.
   // UWAGA: Pamietajcie o kolejnosci
- Serial.begin(9600);
+  Serial.begin(9600);
   dht.setup(DHT11_PIN);
+  // DO ZROBIENIA:
+  // - wlaczenie GPS
+  // - ustawienie GPS w tryb duzych predkosci
+  // - wlaczenie innych sensorow
 }
 
 //funkcja pomiaru temperatury i wilgotnosci
 void pomiardht()
 {
-  float wilgotnosc = dht.getHumidity();
-  float temperatura = dht.getTemperature();
+  wilgotnosc = dht.getHumidity();
+  temperatura = dht.getTemperature();
   
   if (dht.getStatusString() == "OK") {
     Serial.print("Wilgotnosc: ");
@@ -33,6 +40,15 @@ void loop() {
   // tutaj umieszczamy wszystkie funkcje, ktore
   // program ma wykonywac w petli przez caly czas
   // dzialania.
-pomiardht();
-delay (1000);
+
+  // Uruchomienie funkcji odczytujacych dane z sensorow
+  pomiardht();
+
+  // stworzy ramke z wszytkich pomiarow
+  // DO ZROBIENIA
+  
+  // wyslac ramke przez radio na ziemie
+  // DO ZROBIENIA
+
+  delay (1000);
 }
